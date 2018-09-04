b8aaaabc9d699b639e4978bee049a7757cbb1eff
rossi56 <rossi56@hotmail.fr>
2018-09-04 12:05:18 +0200

    placement logo header

---
+++ js/map.js
@@ -0,0 +1,18 @@
+  function initMap() {
+  	map = new google.maps.Map(document.getElementById('map'), {
+  		center: {
+  			lat: 47.675855,
+  			lng: -2.765641
+			},
+  		zoom: 15
+  	});
+  	marker = new google.maps.Marker({
+  		position: map.center,
+  		map: map,
+  		icon: "../images/logos/icon.png"
+  	});
+
+  	marker.setAnimation(google.maps.Animation.BOUNCE); //Animation des marqueurs
+
+
+  }
