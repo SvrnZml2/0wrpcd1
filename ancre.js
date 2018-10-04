81da2e643c224346109c94af059625d1992cdb42
rossi56 <rossi56@hotmail.fr>
2018-10-04 10:34:00 +0200

    offres

--- ancre.js
+++
@@ -1,8 +0,0 @@
-$('ul.nav a').on('click', function(e) {
-    e.preventDefault();
-    var target = $(this).attr('href');
-    var pos = $(target).parents('.page').position().left;
-    var scrollPercent = (pos / ($('.front.scroll').width() - window.innerWidth)).toFixed(4);
-    var top = scrollPercent * ($('body').height() - window.innerHeight);
-    $('html, body').animate({scrollTop: top+'px'}, 200);
-    });
